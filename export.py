1c97c7635aafda767c3b3262e3c11dcb3e6a9188
Kelly Guo <guojiaxuan2001@gmail.com>
2025-04-14 17:06:28 +0900

    updated export

--- export.py
+++ export.py
@@ -8,10 +8,6 @@ scope = [
     "https://www.googleapis.com/auth/drive"
 ]
 
-creds = ServiceAccountCredentials.from_json_keyfile_name("capable-sled-456409-b6-8f3c678b6683.json", scope)
-client = gspread.authorize(creds)
-
-
 cred_path = os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
 creds = ServiceAccountCredentials.from_json_keyfile_name(cred_path, scope)
 client = gspread.authorize(creds)
