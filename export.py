6a571b0d64006dab8cca5487973087813ba7ffd7
Kelly Guo <guojiaxuan2001@gmail.com>
2025-04-14 16:53:17 +0900

    yml updated

--- export.py
+++ export.py
@@ -11,5 +11,10 @@ scope = [
 creds = ServiceAccountCredentials.from_json_keyfile_name("capable-sled-456409-b6-8f3c678b6683.json", scope)
 client = gspread.authorize(creds)
 
+
+cred_path = os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
+creds = ServiceAccountCredentials.from_json_keyfile_name(cred_path, scope)
+client = gspread.authorize(creds)
+
 sheet = client.open("Ikonoijoi").sheet1  
-sheet.append_row(["account", "time", "text"])  
+sheet.append_row(["account", "time", "text"]) 
