6a4db88bb46e4e8f8988df85cc87999e47facca5
Kelly Guo <guojiaxuan2001@gmail.com>
2025-04-14 15:57:16 +0900

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -3,4 +3,7 @@ from following import keywords
 def filter_by_keywords(tweets):
     result = []
     for tweet in tweets:
-        
+        text = tweet["text"]
+        if any(kw in text for kw in keywords):
+            result.append(tweet)
+    return result
